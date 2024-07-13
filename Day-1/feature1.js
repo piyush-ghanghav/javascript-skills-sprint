7acb785ae7cd1180bbe857829bff6c4cf41b8183
Piyush Ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2024-07-13 13:54:03 +0530

    Create feature1.js

---
+++ Day-1/feature1.js
@@ -0,0 +1,13 @@
+// Feature 1: Variable Types Console Log
+let num = 42;
+let str = "JavaScript";
+let bool = false;
+let obj = { name: "Piyush", age: 20 };
+let arr = [1, 2, 3];
+
+console.log('Feature 1:');
+console.log('num:', num, typeof num);
+console.log('str:', str, typeof str);
+console.log('bool:', bool, typeof bool);
+console.log('obj:', obj, typeof obj);
+console.log('arr:', arr, typeof arr);

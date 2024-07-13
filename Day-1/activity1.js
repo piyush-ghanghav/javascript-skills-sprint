2a3100bdadb5c98243bc907e7ace5be4fda66ca3
Piyush Ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2024-07-13 13:52:31 +0530

    Update activity1.js

--- Day-1/activity1.js
+++ Day-1/activity1.js
@@ -1,5 +1,52 @@
+//Activity 1
 var num = 10;
 console.log("Task 1:",num);
 
 let str = "Piyush";
 console.log("Task 2:",str);
+
+//Activity 2
+
+const z = true;
+console.log(z);
+
+
+// Activity 3
+// create variable (number, string, boolean, object, array)
+let num1 = 12;
+let num2 = 1.2;
+let num3 = -12;
+console.log("Num1",typeof(num2));
+console.log("Num2",typeof(num1));
+console.log("Num3",typeof(num3));
+
+
+let str = "Assignment 1"
+console.log("str:",typeof(str));
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1);
+console.log(bool2);
+
+let car = {
+    type : "BMW",
+    color: "Blue",
+    model: 500
+};
+
+console.log(typeof(car));
+
+let arr = [1,2,3,4];
+console.log(typeof(arr));
+
+ y = "xyz";
+
+ console.log(y)
+
+ const v1 = 10;
+ console.log(v1);
+
+//  v1 = 100;   //TypeError: Assignment to constant variable.
+//  console.log(v1); 

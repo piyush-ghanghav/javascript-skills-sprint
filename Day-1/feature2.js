8cbd3322461c53aad358b5e13a71b8f5961744ea
Piyush Ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2024-07-13 13:58:14 +0530

    Create feature2.js

---
+++ Day-1/feature2.js
@@ -0,0 +1,15 @@
+// Feature 2: Reassignment Demo
+
+let reas = 100;
+console.log('Initial Value:', reas);
+reas = 200;
+console.log('New Value:', reas);
+
+// Using const
+const Const = "CONST";
+console.log('Initial Value:', immutableConst);
+
+// Const = "NEWCONST"; // 
+
+ //TypeError: Assignment to constant variable.
+//  console.log(Const); 
